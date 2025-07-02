904a07eba1c9876c510d60ead79a8199bde839ea
Anusara97 <anusara777rcm@gmail.com>
2025-07-02 15:44:00 +0530

    push() method is created.

--- Stack.java
+++ Stack.java
@@ -6,4 +6,20 @@ public class Stack {
         stk = new int[10];
         top = -1;
     }
+
+    public boolean isEmpty() {
+        return top == -1;
+    }
+
+    public boolean isFull() {
+        return top == stk.length - 1;
+    }
+
+    public void push(int item) {
+        if (isFull()) {
+            System.out.println("Stack is overflow.");
+        } else {
+            stk[++top] = item;
+        }
+    }
 }
